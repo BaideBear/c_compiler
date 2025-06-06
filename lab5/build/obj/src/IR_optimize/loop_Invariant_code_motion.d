dc8dcd67f6752de2fce17afab28db524580ccd13
auroralbeauty <413917185@qq.com>
2025-06-06 16:31:22 +0800

    refix som bugs

--- lab5/build/obj/src/IR_optimize/loop_Invariant_code_motion.d
+++
@@ -1,6 +0,0 @@
-build/obj/./src/IR_optimize/loop_Invariant_code_motion.o: \
- src/IR_optimize/loop_Invariant_code_motion.c \
- src/IR_optimize/include/loop_invariant_code_motion.h \
- src/IR_optimize/include/dataflow_analysis.h include/IR.h \
- include/object.h include/macro.h include/container/vector.h \
- include/container/list.h include/container/treap.h
